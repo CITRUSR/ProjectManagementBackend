7dc5c04bf0ef4dbdc62ca534772141ce420d7a43
Citrus <savelijpetrov38@gmail.com>
2024-01-24 22:51:51 +0300

    Initial commit

---
+++ Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Application.Common.Behaviors;
+
+public class ValidationBehavior<TRequest,TResponse> : IPipelineBehavior<TRequest,TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly List<IValidator> _validators;
+
+    public ValidationBehavior(List<IValidator> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var context = new ValidationContext<TRequest>(request);
+
+        var errors = _validators.Select(validator => validator.Validate(context))
+            .Where(result => !result.IsValid)
+            .SelectMany(failures => failures.Errors)
+            .ToList();
+        
+        if (errors.Any())
+        {
+            throw new ValidationException(errors);
+        }
+
+        return await next();
+    }
+}
---
+++ Presentation/Program.cs
@@ -0,0 +1,22 @@
+using Application;
+using Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication()
+    .AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
