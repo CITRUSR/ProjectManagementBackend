9b2d5d13bb8f709405d22eb74c80a12ad0cbae77
Citrus <savelijpetrov38@gmail.com>
2024-01-29 23:33:13 +0300

    Add validation pipeline

--- Application/Common/Behaviors/ValidationBehavior.cs
+++ Application/Common/Behaviors/ValidationBehavior.cs
@@ -5,9 +5,9 @@ namespace Application.Common.Behaviors;
 
 public class ValidationBehavior<TRequest,TResponse> : IPipelineBehavior<TRequest,TResponse> where TRequest : IRequest<TResponse>
 {
-    private readonly List<IValidator> _validators;
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
 
-    public ValidationBehavior(List<IValidator> validators)
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
     {
         _validators = validators;
     }
