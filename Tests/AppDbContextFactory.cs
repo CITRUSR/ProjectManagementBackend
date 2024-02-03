502b63524504be1d09f228d1aea20ba89738643f
Citrus <savelijpetrov38@gmail.com>
2024-02-03 23:34:30 +0300

    Add tests

---
+++ Tests/AppDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Enum;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests;
+
+public class AppDbContextFactory
+{
+    public static AppDbContext Create()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        AppDbContext dbContext = new AppDbContext(options);
+
+        dbContext.Database.EnsureCreated();
+
+        dbContext.SaveChanges();
+
+        return dbContext;
+    }
+
+    public static void Destroy(AppDbContext context)
+    {
+        context.Database.EnsureDeleted();
+        context.Dispose();
+    }
+}
---
+++ Tests/Context.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure;
+
+namespace Tests;
+
+public class Context : IDisposable
+{
+    protected AppDbContext DbContext;
+
+    public Context()
+    {
+        DbContext = AppDbContextFactory.Create();
+    }
+
+    public void Dispose()
+    {
+        AppDbContextFactory.Destroy(DbContext);
+    }
+}
