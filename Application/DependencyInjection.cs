f9cb7f62389c4796f480622a2d512acf1e96662f
Citrus <savelijpetrov38@gmail.com>
2024-02-07 23:18:09 +0300

    Add mappers to di

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using Application.Auth;
 using Application.Common.Behaviors;
+using Application.Common.Mappers;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,17 +13,21 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         var assembly = typeof(DependencyInjection).Assembly;
-        
+
         services.AddSingleton<AuthOptions>();
         services.AddScoped<JWTGenerator>();
-        
+
         services.AddMediatR(configuration =>
             configuration.RegisterServicesFromAssembly(assembly));
-        
+
         services.AddValidatorsFromAssembly(assembly);
 
+        services.AddSingleton<ProjectMapper>();
+        services.AddSingleton<TaskMapper>();
+        services.AddSingleton<UserMapper>();
+
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-        
+
         return services;
     }
 }
