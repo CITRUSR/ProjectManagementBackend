a87ff93a7a7d0e7865af0596d73c9978df643786
Citrus <savelijpetrov38@gmail.com>
2024-02-13 22:24:06 +0300

    Change db to localDb

--- Infrastructure/AppDbContext.cs
+++ Infrastructure/AppDbContext.cs
@@ -13,6 +13,7 @@ public class AppDbContext : IdentityDbContext<AppUser>, IAppDbContext
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
+        Database.EnsureCreated();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
